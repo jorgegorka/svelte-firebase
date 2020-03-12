45cdd4acfc8083d85053fcd932951bc679fba53d
AJ Caldwell <tomatrow@gmail.com>
2020-03-12 14:14:45 -0700

    Fix missing space

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import { Router } from 'svelte-router-spa'
-  import {routes } from './routes.js'
+  import { routes } from './routes.js'
   import '../node_modules/materialize-css/dist/css/materialize.min.css'
   import '../node_modules/materialize-css/dist/js/materialize.min.js'
 </script>
