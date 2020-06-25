bf918c061565d50bc4d89511eaa87d9846837004
Jorge Alvarez <jorge.alvarez@nb-jalvarez-2.local>
2020-06-25 19:22:17 +0200

    Upgrade packages.

--- src/lib/routes/public.js
+++ src/lib/routes/public.js
@@ -2,14 +2,16 @@ import Login from '../../views/public/login/index.svelte'
 import Signup from '../../views/public/signup/index.svelte'
 import PublicIndex from '../../views/public/home/index.svelte'
 import PublicLayout from '../../views/public/layout/index.svelte'
+import NotFound from '../../views/404.svelte'
 
 const publicRoutes = [
   {
     name: '/',
-    component: PublicIndex
+    component: PublicIndex,
   },
   { name: 'login', component: Login, layout: PublicLayout },
-  { name: 'signup', component: Signup, layout: PublicLayout }
+  { name: 'signup', component: Signup, layout: PublicLayout },
+  { name: '404', component: NotFound, layout: PublicLayout },
 ]
 
 export { publicRoutes }
